c4523cec273bce5f27d1b0648834aecd6b65c05d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-06-17 18:03:35 +0900

    수정

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request) : 
-    return HttpResponse("pybo")
+    return HttpResponse("pybo입니다.")
 
 # Create your views here.
